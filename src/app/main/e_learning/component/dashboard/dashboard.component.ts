9773935c8ab2ce22ea75cdab7c029ea513bf534a
BOURHOU Abdelilah <abdou.loujdi7@gmail.com>
2023-01-15 14:20:42 +0100

    Some update

--- src/app/main/e_learning/component/dashboard/dashboard.component.ts
+++ src/app/main/e_learning/component/dashboard/dashboard.component.ts
@@ -8,8 +8,6 @@ import { colors } from 'app/colors.const';
 })
 export class DashboardComponent implements OnInit {
 
-  public goalChartoptions;
-
   coursCount : number;
   commentCount : number;
   positive : number;
@@ -17,79 +15,7 @@ export class DashboardComponent implements OnInit {
 
   searchText : string;
 
-  goalOverview: {
-    series: [83],
-    analyticsData: {
-      completed: '786,617',
-      inProgress: '13,561'
-    }
-  }
-
   constructor() {
-
-    // Goal Overview  Chart
-    this.goalChartoptions = {
-      chart: {
-        height: 245,
-        type: 'radialBar',
-        sparkline: {
-          enabled: true
-        },
-        dropShadow: {
-          enabled: true,
-          blur: 3,
-          left: 1,
-          top: 1,
-          opacity: 0.1
-        }
-      },
-      colors: [this.$goalStrokeColor2],
-      plotOptions: {
-        radialBar: {
-          offsetY: -10,
-          startAngle: -150,
-          endAngle: 150,
-          hollow: {
-            size: '77%'
-          },
-          track: {
-            background: this.$strokeColor,
-            strokeWidth: '50%'
-          },
-          dataLabels: {
-            name: {
-              show: false
-            },
-            value: {
-              color: this.$textHeadingColor,
-              fontSize: '2.86rem',
-              fontWeight: '600'
-            }
-          }
-        }
-      },
-      fill: {
-        type: 'gradient',
-        gradient: {
-          shade: 'dark',
-          type: 'horizontal',
-          shadeIntensity: 0.5,
-          gradientToColors: [colors.solid.success],
-          inverseColors: true,
-          opacityFrom: 1,
-          opacityTo: 1,
-          stops: [0, 100]
-        }
-      },
-      stroke: {
-        lineCap: 'round'
-      },
-      grid: {
-        padding: {
-          bottom: 30
-        }
-      }
-    };
 
   }
 
@@ -99,30 +25,23 @@ export class DashboardComponent implements OnInit {
     this.positive = 90;
     this.negative = 10;
 
-    this.goalOverview = {
-      series: [83],
-      analyticsData: {
-        completed: '786,617',
-        inProgress: '13,561'
-      }
-    }
 
     this.getCours();
   }
 
   coursP : number;
   coursN : number;
-  avg : number;
+  avgP : number = 50;
+  avgN : number;
+  total : number;
   getCours(){
     this.coursP = 20;
     this.coursN = 6;
-    this.avg = this.coursP / (this.coursP + this.coursN);
+    this.total = this.coursN + this.coursP;
+    this.avgP = (this.coursP / this.total) * 100;
+    this.avgN = (this.coursN / this.total) * 100;
   }
 
-
-  private $goalStrokeColor2 = '#51e5a8';
-  private $textHeadingColor = '#5e5873';
-  private $strokeColor = '#ebe9f1';
   
 }
 
